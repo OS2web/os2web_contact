35538e744dd9c65a6cc7df6ef9cdd7f9a09024ca
Stanislav Kutasevits <stan@bellcom.dk>
2020-09-03 11:56:22 +0300

    Making contact block work without menu item

--- src/Plugin/Block/ContactBlock.php
+++ src/Plugin/Block/ContactBlock.php
@@ -4,6 +4,7 @@ namespace Drupal\os2web_contact\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\node\Entity\Node;
+use Drupal\node\NodeInterface;
 
 /**
  * Provides a 'OS2Web Contact' block.
@@ -20,36 +21,66 @@ class ContactBlock extends BlockBase {
    */
   public function build() {
     $block = NULL;
-    $hide_contact = FALSE;
-    $ids = array_filter(\Drupal::service('menu.active_trail')->getActiveTrailIds(NULL));
     $contact_entity = NULL;
-    while ($id = array_shift($ids)) {
-      /** @var \Drupal\menu_link_content\Plugin\Menu\MenuLinkContent $menuLink */
-      $menuLink = \Drupal::service('plugin.manager.menu.link')->createInstance($id);
-      $urlObject = $menuLink->getUrlObject();
-      // Skipping external link menu items.
-      if ($urlObject->isExternal()) {
-        continue;
+
+    $node = \Drupal::routeMatch()->getParameter('node');
+    if ($node && $node->bundle() == 'os2web_page') {
+      // Checking visibility.
+      if (!$this->checkContactBlockVisible($node)) {
+        return NULL;
       }
-      $routeName = $urlObject->getRouteName();
-      $routeParams = $urlObject->getRouteParameters();
-      if ($routeName == 'entity.node.canonical') {
-        $node = Node::load($routeParams['node']);
-        if ($node && $node->bundle() == 'os2web_page') {
-          if ($node->field_os2web_page_contact_hide->value) {
-            $hide_contact = TRUE;
+
+      // Getting this node contact block.
+      $contact_entity = $this->getNodeContactBlock($node);
+
+      // No contact block found, look up the menu for contact block.
+      if (!$contact_entity) {
+        /** @var \Drupal\Core\Menu\MenuLinkManagerInterface $menu_link_manager */
+        $menu_link_manager = \Drupal::service('plugin.manager.menu.link');
+
+        $links = $menu_link_manager->loadLinksByRoute('entity.node.canonical', ['node' => $node->id()]);
+
+        // Getting current menu link.
+        $activeLink = end($links);
+        $parentLinkId = $activeLink->getParent();
+
+        // Traversing through all menus link up the tree.
+        while ($parentLinkId) {
+          /** @var \Drupal\menu_link_content\Plugin\Menu\MenuLinkContent $parentLink */
+          $parentLink = $menu_link_manager->createInstance($parentLinkId);
+          $urlObject = $parentLink->getUrlObject();
+
+          // Skipping external link menu items.
+          if ($urlObject->isExternal()) {
+            continue;
           }
-          if ($node->hasField('field_os2web_page_contact') && !$node->field_os2web_page_contact->isEmpty()) {
-            $contact_entity = $node->field_os2web_page_contact->referencedEntities()[0];
-            break;
+
+          if ($urlObject->getRouteName() == 'entity.node.canonical') {
+            $routeParams = $urlObject->getRouteParameters();
+            $node = Node::load($routeParams['node']);
+
+            if ($node && $node->bundle() == 'os2web_page') {
+              // Checking visibility.
+              if (!$this->checkContactBlockVisible($node)) {
+                return NULL;
+              }
+
+              if ($contact_entity = $this->getNodeContactBlock($node)) {
+                break;
+              }
+            }
           }
+
+          $parentLinkId = $parentLink->getParent();
         }
       }
     }
-    if ($contact_entity && !$hide_contact) {
+
+    if ($contact_entity) {
       $view_builder = \Drupal::entityTypeManager()->getViewBuilder('os2web_contact');
       $block = $view_builder->view($contact_entity);
     }
+
     return $block;
   }
 
@@ -58,6 +89,38 @@ class ContactBlock extends BlockBase {
    */
   public function getCacheMaxAge() {
     return 0;
+  }
+
+  /**
+   * Gets the contact block associated with the node.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node to find block in.
+   *
+   * @return mixed
+   *   Referenced block entity or NULL.
+   */
+  protected function getNodeContactBlock(NodeInterface $node) {
+    if ($node->hasField('field_os2web_page_contact') && !$node->field_os2web_page_contact->isEmpty()) {
+      return $node->field_os2web_page_contact->referencedEntities()[0];
+    }
+  }
+
+  /**
+   * Checks if the block is set to be visible.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   Node to check against.
+   *
+   * @return bool
+   *   TRUE is visible, FALSE otherwise.
+   */
+  protected function checkContactBlockVisible(NodeInterface $node) {
+    if ($node->field_os2web_page_contact_hide->value) {
+      return FALSE;
+    }
+
+    return TRUE;
   }
 
 }
